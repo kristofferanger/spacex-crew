0fa46bba271f3827c7cac50d355dc815caf9e4a8
Kristoffer Anger <kristoffer.anger@gmail.com>
2023-08-20 15:46:33 +0200

    Initial Commit

---
+++ SpaceXCrew/SpaceXCrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXCrewApp.swift
+//  SpaceXCrew
+//
+//  Created by Kristoffer Anger on 2023-08-20.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXCrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
